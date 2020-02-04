b866ce438a71644137520d28e227bd45300000b8
Yu Xuan Liu <andrew@embody.ai>
2020-02-03 22:05:56 -0800

    misc opt

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -29,11 +29,11 @@ static void inline do_block_naive(int lda, int M, int N, int K, double *A, doubl
     }
 }
 
-static void inline do_block(int lda, int M, int N, int K, double *A, double *B, double *C) {
-    for (int i = 0; i < M; ++i) {  // Row iteration
-        for (int j = 0; j < N; j += 4) {  // Column iteration
+static void inline do_block(int lda, double *A, double *B, double *C) {
+    for (int i = 0; i < BLOCK_SIZE; ++i) {  // Row iteration
+        for (int j = 0; j < BLOCK_SIZE; j += 4) {  // Column iteration
             __m256d Cval = _mm256_loadu_pd(C + i * lda + j);
-            for (int k = 0; k < K; ++k) {
+            for (int k = 0; k < BLOCK_SIZE; ++k) {
                 __m256d Aval = _mm256_set1_pd(A[i * lda + k]);
                 __m256d Bval = _mm256_loadu_pd(B + k * lda + j);
                 Cval = _mm256_fmadd_pd(Aval, Bval, Cval);
@@ -66,10 +66,10 @@ void square_dgemm(int lda, double *A, double *B, double *C) {
 
     // For each block-row of A
     for (int i = 0; i < lda; i += BLOCK_SIZE) {
-        // For each block-column of B
-        for (int j = 0; j < lda; j += BLOCK_SIZE) {
             // Accumulate block dgemms into block of C
             for (int k = 0; k < lda; k += BLOCK_SIZE) {
+        // For each block-column of B
+        for (int j = 0; j < lda; j += BLOCK_SIZE) {
                 // Correct block dimensions if block "goes off edge of" the matrix
                 int M = min(BLOCK_SIZE, lda - i);
                 int N = min(BLOCK_SIZE, lda - j);
@@ -78,7 +78,7 @@ void square_dgemm(int lda, double *A, double *B, double *C) {
                 if ((M == BLOCK_SIZE) &&
                     (N == BLOCK_SIZE) &&
                     (K == BLOCK_SIZE)) {
-                    do_block(lda, M, N, K, A_row + i * lda + k, B_row + k * lda + j, C_row + i * lda + j);
+                    do_block(lda, A_row + i * lda + k, B_row + k * lda + j, C_row + i * lda + j);
                 } else {
                     do_block_naive(lda, M, N, K, A_row + i * lda + k, B_row + k * lda + j, C_row + i * lda + j);
                 }
