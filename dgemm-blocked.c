19df72046bdf34e6407c14b17a00d3a4c82445b1
Yu Xuan Liu <andrew@embody.ai>
2020-02-03 23:01:02 -0800

    padding

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -6,28 +6,8 @@ const char *dgemm_desc = "Simple blocked dgemm.";
 #define BLOCK_SIZE 8
 #endif
 
-
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
-/*
- * This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N.
- */
-static void inline do_block_naive(int lda, int M, int N, int K, double *A, double *B, double *C) {
-    // For each row i of A
-    for (int i = 0; i < M; ++i) {
-        //For each column j of B
-        for (int j = 0; j < N; ++j) {
-            // Compute C(i,j)
-            double cij = C[i * lda + j];
-            for (int k = 0; k < K; ++k) {
-                cij += A[i * lda + k] * B[k * lda + j];
-            }
-            C[i * lda + j] = cij;
-        }
-    }
-}
 
 static void inline do_block(int lda, double *A, double *B, double *C) {
     for (int i = 0; i < BLOCK_SIZE; ++i) {  // Row iteration
@@ -44,10 +24,11 @@ static void inline do_block(int lda, double *A, double *B, double *C) {
 }
 
 // Convert column major to row major.
-static void inline swap_ordering(int N, double *src, double *tgt) {
+static void inline swap_ordering(int N, double *src, double *tgt, 
+    int src_skip, int tgt_skip) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            tgt[i * N + j] = src[i + j * N];
+            tgt[i * tgt_skip + j] = src[i + j * src_skip];
         }
     }
 }
@@ -57,35 +38,31 @@ static void inline swap_ordering(int N, double *src, double *tgt) {
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */
 void square_dgemm(int lda, double *A, double *B, double *C) {
-    double *A_row = (double *) malloc(lda * lda * sizeof(double));
-    swap_ordering(lda, A, A_row);
-    double *B_row = (double *) malloc(lda * lda * sizeof(double));
-    swap_ordering(lda, B, B_row);
-    double *C_row = (double *) malloc(lda * lda * sizeof(double));
-    swap_ordering(lda, C, C_row);
+
+    int rounded = (lda + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
+
+    double *A_row = (double *) calloc(rounded * rounded , sizeof(double));
+    swap_ordering(lda, A, A_row, lda, rounded);
+    double *B_row = (double *) calloc(rounded * rounded ,sizeof(double));
+    swap_ordering(lda, B, B_row, lda, rounded);
+    double *C_row = (double *) malloc(rounded * rounded * sizeof(double));
+    swap_ordering(lda, C, C_row, lda, rounded);
 
     // For each block-row of A
     for (int i = 0; i < lda; i += BLOCK_SIZE) {
-            // Accumulate block dgemms into block of C
-            for (int k = 0; k < lda; k += BLOCK_SIZE) {
-        // For each block-column of B
-        for (int j = 0; j < lda; j += BLOCK_SIZE) {
+        // Accumulate block dgemms into block of C
+        for (int k = 0; k < lda; k += BLOCK_SIZE) {
+            // For each block-column of B
+            for (int j = 0; j < lda; j += BLOCK_SIZE) {
                 // Correct block dimensions if block "goes off edge of" the matrix
-                int M = min(BLOCK_SIZE, lda - i);
-                int N = min(BLOCK_SIZE, lda - j);
-                int K = min(BLOCK_SIZE, lda - k);
-                // Perform individual block dgemm
-                if ((M == BLOCK_SIZE) &&
-                    (N == BLOCK_SIZE) &&
-                    (K == BLOCK_SIZE)) {
-                    do_block(lda, A_row + i * lda + k, B_row + k * lda + j, C_row + i * lda + j);
-                } else {
-                    do_block_naive(lda, M, N, K, A_row + i * lda + k, B_row + k * lda + j, C_row + i * lda + j);
-                }
+                do_block(rounded, 
+                    A_row + i * rounded + k, 
+                    B_row + k * rounded + j, 
+                    C_row + i * rounded + j);
             }
         }
     }
-    swap_ordering(lda, C_row, C);
+    swap_ordering(lda, C_row, C, rounded, lda);
     free(C_row);
     free(B_row);
     free(A_row);
