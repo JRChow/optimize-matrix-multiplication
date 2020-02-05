de6f2361c8318e93eadb07221e85a7f783727a7b
Jerome <zjrtomjohn@hotmail.com>
2020-02-05 00:16:12 -0800

    Partial update (careful)

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -2,20 +2,90 @@
 
 const char *dgemm_desc = "Simple blocked dgemm.";
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE 12
+#ifndef BLOCK_SIZE_L2
+#define BLOCK_SIZE_L2 128
+#define BLOCK_SIZE_L1 64
+#define DIVISOR 12
 #endif
 
-static void inline do_block(int lda, double *A, double *B, double *C) {
-    for (int j = 0; j < BLOCK_SIZE; j++) {  // Column iteration
-        for (int i = 0; i < BLOCK_SIZE; i += 4) {  // Row iteration
-            __m256d Cval = _mm256_loadu_pd(C + i + j * lda);
-            for (int k = 0; k < BLOCK_SIZE; ++k) {
-                __m256d Aval = _mm256_loadu_pd(A + i + k * lda);
-                __m256d Bval = _mm256_set1_pd(B[k + j * lda]);
-                Cval = _mm256_fmadd_pd(Aval, Bval, Cval);
+#define min(a, b) (((a)<(b))?(a):(b))
+
+// Register blocking
+static void inline do_block_register(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    for (int i = 0; i < M; i += 3) {
+        for (int j = 0; j < N; j += 12) {
+            // Column 0 of C
+            register __m256d C_00 = _mm256_loadu_pd(C + (i + 0) * lda + j + 0);
+            register __m256d C_10 = _mm256_loadu_pd(C + (i + 0) * lda + j + 4);
+            register __m256d C_20 = _mm256_loadu_pd(C + (i + 0) * lda + j + 8);
+            // Column 1 of C
+            register __m256d C_01 = _mm256_loadu_pd(C + (i + 1) * lda + j + 0);
+            register __m256d C_11 = _mm256_loadu_pd(C + (i + 1) * lda + j + 4);
+            register __m256d C_21 = _mm256_loadu_pd(C + (i + 1) * lda + j + 8);
+            // Column 2 of C
+            register __m256d C_02 = _mm256_loadu_pd(C + (i + 2) * lda + j + 0);
+            register __m256d C_12 = _mm256_loadu_pd(C + (i + 2) * lda + j + 4);
+            register __m256d C_22 = _mm256_loadu_pd(C + (i + 2) * lda + j + 8);
+
+            for (int k = 0; k < K; k++) {
+                register __m256d b_00_03 = _mm256_loadu_pd(B + k * lda + j * 12);
+                register __m256d b_00_03 = _mm256_loadu_pd(B + k * lda + j * 12);
+                register __m256d b_00_03 = _mm256_loadu_pd(B + k * lda + j * 12);
+                register __m256d a00 = _mm256_broadcast_sd(A + (4 * i) * lda + k);
+
+
+
             }
-            _mm256_storeu_pd(C + i + j * lda, Cval);
+        }
+    }
+}
+
+//static void inline do_block_l1(int lda, int M, int N, int K, double *A, double *B, double *C) {
+//    for (int j = 0; j < BLOCK_SIZE_L1; j++) {  // Column iteration
+//        for (int i = 0; i < BLOCK_SIZE_L1; i += 4) {  // Row iteration
+//            __m256d Cval = _mm256_loadu_pd(C + i + j * lda);
+//            for (int k = 0; k < BLOCK_SIZE_L1; ++k) {
+//                __m256d Aval = _mm256_loadu_pd(A + i + k * lda);
+//                __m256d Bval = _mm256_set1_pd(B[k + j * lda]);
+//                Cval = _mm256_fmadd_pd(Aval, Bval, Cval);
+//            }
+//            _mm256_storeu_pd(C + i + j * lda, Cval);
+//        }
+//    }
+//}
+
+// Level-1 blocking
+static void inline do_block_l1(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    for (int j = 0; j < N; j += BLOCK_SIZE_L1) {
+        for (int k = 0; k < K; k += BLOCK_SIZE_L1) {
+            for (int i = 0; i < M; i += BLOCK_SIZE_L1) {
+                /* Correct block dimensions if block "goes off edge of" the matrix */
+                int M_reg = min (BLOCK_SIZE_L2, M - i);
+                int N_reg = min (BLOCK_SIZE_L2, N - j);
+                int K_reg = min (BLOCK_SIZE_L2, K - k);
+                do_block_register(lda, M_reg, N_reg, K_reg,
+                            A + i + k * lda,
+                            B + k + j * lda,
+                            C + i + j * lda);
+            }
+        }
+    }
+}
+
+// Level-2 blocking
+static void inline do_block_l2(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    for (int j = 0; j < N; j += BLOCK_SIZE_L1) {
+        for (int k = 0; k < K; k += BLOCK_SIZE_L1) {
+            for (int i = 0; i < M; i += BLOCK_SIZE_L1) {
+                /* Correct block dimensions if block "goes off edge of" the matrix */
+                int M_L1 = min (BLOCK_SIZE_L2, M - i);
+                int N_L1 = min (BLOCK_SIZE_L2, N - j);
+                int K_L1 = min (BLOCK_SIZE_L2, K - k);
+                do_block_l1(lda, M_L1, N_L1, K_L1,
+                            A + i + k * lda,
+                            B + k + j * lda,
+                            C + i + j * lda);
+            }
         }
     }
 }
@@ -34,33 +104,38 @@ static void inline padding(int N, const double *src, double *tgt, int src_skip, 
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */
 void square_dgemm(int lda, double *A, double *B, double *C) {
+    // Determine padding
+    int lda_pad = lda;
+    int r = lda % DIVISOR;
+    if (r != 0) {
+        lda_pad += DIVISOR - r;
+    }
 
-    int rounded = (lda + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
+    double *A_pad = (double *) _mm_malloc(lda_pad * lda_pad * sizeof(double), 32);
+    padding(lda, A, A_pad, lda, lda_pad);
+    double *B_pad = (double *) _mm_malloc(lda_pad * lda_pad * sizeof(double), 32);
+    padding(lda, B, B_pad, lda, lda_pad);
+    double *C_pad = (double *) _mm_malloc(lda_pad * lda_pad * sizeof(double), 32);
+    padding(lda, C, C_pad, lda, lda_pad);
 
-    double *A_row = (double *) calloc(rounded * rounded, sizeof(double));
-    padding(lda, A, A_row, lda, rounded);
-    double *B_row = (double *) calloc(rounded * rounded, sizeof(double));
-    padding(lda, B, B_row, lda, rounded);
-    double *C_row = (double *) malloc(rounded * rounded * sizeof(double));
-    padding(lda, C, C_row, lda, rounded);
-
-    // For each block-column of B
-    for (int j = 0; j < lda; j += BLOCK_SIZE) {
-        // Accumulate block dgemms into block of C
-        for (int k = 0; k < lda; k += BLOCK_SIZE) {
-            // For each block-row of A
-            for (int i = 0; i < lda; i += BLOCK_SIZE) {
-                // Correct block dimensions if block "goes off edge of" the matrix
-                do_block(rounded,
-                         A_row + i + k * rounded,
-                         B_row + k + j * rounded,
-                         C_row + i + j * rounded);
+    for (int j = 0; j < lda_pad; j += BLOCK_SIZE_L2) {
+        for (int k = 0; k < lda_pad; k += BLOCK_SIZE_L2) {
+            for (int i = 0; i < lda_pad; i += BLOCK_SIZE_L2) {
+                /* Correct block dimensions if block "goes off edge of" the matrix */
+                int M = min (BLOCK_SIZE_L2, lda_pad - i);
+                int N = min (BLOCK_SIZE_L2, lda_pad - j);
+                int K = min (BLOCK_SIZE_L2, lda_pad - k);
+                do_block_l2(lda_pad, M, N, K,
+                            A_pad + i + k * lda_pad,
+                            B_pad + k + j * lda_pad,
+                            C_pad + i + j * lda_pad);
             }
         }
     }
 
-    padding(lda, C_row, C, rounded, lda);
-    free(C_row);
-    free(B_row);
-    free(A_row);
+    // Un-pad C
+    padding(lda, C_pad, C, lda_pad, lda);
+    free(C_pad);
+    free(B_pad);
+    free(A_pad);
 }
